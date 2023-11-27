f11d4de4c961f22fcd985bec26fa41e252978d3f
dinhanhquoc <anhquoc004@gmail.com>
2023-11-27 15:19:16 +0700

    Test Changes Code

--- WebDevelop/Areas/Admin/Controllers/DashboardController.cs
+++ WebDevelop/Areas/Admin/Controllers/DashboardController.cs
@@ -13,5 +13,9 @@ namespace WebDevelop.Areas.Admin.Controllers
         {
             return View();
         }
+        public ActionResult Test()
+        {
+            return View();
+        }
     }
 }
